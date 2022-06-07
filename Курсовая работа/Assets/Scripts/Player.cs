e906b96d34391c4d76949ed1978c8332d9dc7c90
impullsss <45658912+impullsss@users.noreply.github.com>
2022-06-07 04:16:14 +0300

    По новому все сделал

--- Курсовая работа/Assets/Scripts/Player.cs
+++ Курсовая работа/Assets/Scripts/Player.cs
@@ -2,43 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Character
 {
-    int Speed = 10;
-    float force;
-    public Rigidbody2D rigidbody;
-    int minimalHeight = -50;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+
+    private int extraJumps;
+    public int extraJumpsValue;
 
     private void Start()
     {
-        int applesCount = 3 + 5;
-
-        Debug.Log(applesCount);
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
-    void Update()
+    private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector2.left * Time.deltaTime * Speed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector2.right * Time.deltaTime * Speed);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector2.up * Time.deltaTime * Speed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector2.down * Time.deltaTime * Speed);
-        }
-        if (transform.position.y < minimalHeight)
-        {
-            transform.position = new Vector3(0, 0, 0);  
-        }
-    }
-}
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if(facingRight == false && moveInput>0)
+        {
+            Flip();
+        } else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    private void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+
+    }
+
+}
